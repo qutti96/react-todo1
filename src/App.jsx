0e9b2fe05d19c6a9582cd54551b8090d98078d6a
qutti96 <qutti96@gmail.com>
2024-04-16 16:07:30 +0900

    Added filter function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react"
-import './index.css'
+import { useEffect, useState } from "react"
+import './index.css';
 
 export const App = () =>{
   // todoリスト
   const [todos, setTodos] = useState(
     [
-      {id: 1, title: 'todoリストに着手'},
-      {id: 2, title: 'todoのタスクを洗い出す'}
+      {id: 1, status: "notStarted", title: 'todoリスト未着手'},
+      {id: 2, status: "inProgress", title: 'todoリスト進行中タスク'},
+      {id: 3, status: "done", title: 'todoリスト完了タスク'},
+      {id: 4, status: "done", title: 'todoのタスクを洗い出す'}
     ]
   )
+
   const [todoTitle, setTodoTitle] = useState('')
   const [todoId, setTodoID] = useState(todos.length + 1)
   const [isEditable, setIsEditable] = useState(false)
+  //
+  const [filteredTodos, setFilteredTodos] = useState([])
+ //
+  const [filter, setFilter] = useState("notStarted")
 
   // 問題1. 編集したいtodoのidの状態を定義しよう
   const [editId, setEditId] = useState('')
@@ -22,12 +29,18 @@ export const App = () =>{
     setTodoTitle(e.target.value)
   }
 
-  const addTodos = () => {
-    setTodos([...todos, {id: todoId , title: todoTitle}])
+  const handleAddTodos = () => {
+    setTodos([...todos, {id: todoId , status: filter , title: todoTitle}])
     setTodoID(todoId+1)
+    setFilter('notStarted')
     setTodoTitle('')
   }
-// ・配列todosに、idとタイトルをもつ新しいtodoのオブジェクトを追加
+
+  // const handleSetTodoTitle = (e) => {
+  //   setTodoTitle(e.target.value)
+  // }
+
+  // ・配列todosに、idとタイトルをもつ新しいtodoのオブジェクトを追加
 // ・次回todoを再び作成する時にidが被らないようにidのstateを更新
 
 //関数に削除対象のtodoが渡っているか確認しよう
@@ -69,13 +82,71 @@ export const App = () =>{
   handleCloseEditForm('')
   }
 
+  //
+  const handleStatusChange = ({id},e) =>{
+    const newTodos = todos.map((todo) => ({...todo}));
+    setTodos(
+      newTodos.map((todo) => todo.id === id? {...todo, status:e.target.value} :todo)
+    )
+  }
+
+  useEffect(() => {
+    const filteringTodos = () => {
+      switch(filter){
+        case "notStarted":
+          setFilteredTodos(todos.filter((todo) => todo.status === "notStarted")
+          )
+          break
+        case "inProgress":
+          setFilteredTodos(todos.filter((todo) => todo.status === "inProgress")
+          )
+          break
+        case "done":
+          setFilteredTodos(todos.filter((todo) => todo.status === "done")
+          )
+          break
+          default:
+            setFilteredTodos(todos)
+      }
+    }
+    filteringTodos()
+  },[filter,todos])
+
+
   return(
     <>
     <div class="px-5 py-12 lg:px-20">
-      <div class="w-full max-w-3xl md:w-auto px-10 mx-auto mt-8 my-6 transition duration-500 ease-in-out transform bg-white rounded-lg md:mt-0">
+      <div class="w-full max-w-4xl md:w-auto px-10 mx-auto mt-8 my-6 transition duration-500 ease-in-out transform bg-white rounded-lg md:mt-0">
         <h1 className="font-bold mb-4 text-3xl text-center">Todo アプリ</h1>
           <div class="w-full mx-auto">
-            <div  className="bg-yellow-50 p-8 mb-12">
+            <div className="bg-pink-50 p-8 mb-12">
+              <h2 className="text-2xl font-bold mb-2">todoリストフィルター機能</h2>
+              <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                <option value="all">すべて</option>
+                <option value="notStarted">未着手</option>
+                <option value="inProgress">作業中</option>
+                <option value="done">完了</option>
+              </select>
+
+      <ul>
+        {filteredTodos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.title}</span>
+            <select
+              className="appearance-none rounded-md mr-4 brock p-2"
+              value={todo.status}
+              onChange={(e) => handleStatusChange(todo, e)}
+            >
+              <option value="notStarted">未着手</option>
+              <option value="inProgress">作業中</option>
+              <option value="done">完了</option>
+            </select>
+          </li>
+        ))}
+      </ul>
+
+            </div>
+            <div className="bg-yellow-50 p-8 mb-12">
               <h2 className="text-2xl font-bold mb-2">todoリスト新規作成</h2>
                     {isEditable ? (
                                     <div className="flex items-center flex-wrap">
@@ -92,7 +163,7 @@ export const App = () =>{
                                   ) : (
                                     <div className="flex items-center justify-start mt-4">
                                       <input type="text" name="" id="" label="タイトル" value={todoTitle} onChange={handleAddFormChanges} className="block w-2/5 px-5 py-3 mr-4 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transform border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"/>
-                                      <button onClick={addTodos} className="w-50 px-10 py-4  mr-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-green-700 rounded-xl hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-600">
+                                      <button onClick={handleAddTodos} className="w-50 px-10 py-4  mr-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-green-700 rounded-xl hover:bg-green-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-600">
                                         todo作成！
                                       </button>
                                     </div>
@@ -104,11 +175,13 @@ export const App = () =>{
                 <ul>
                   {todos.map((todo) =>
                   <li key={todo.id} className="flex items-center flex-wrap w-full mb-4">
-                    <span className="block w-2/5 mr-4">{todo.title}</span>
-                    <button onClick={() => handleOpenEditForm(todo)} className="w-50 px-10 py-4  mr-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-700 rounded-xl hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    <span className="mr-1">[id:{todo.id}]</span>
+                    <span className="mr-1">[{todo.status}]</span>
+                    <span className="mr-4 font-bold">{todo.title}</span>
+                    <button onClick={() => handleOpenEditForm(todo)} className="w-30 px-10 py-4  mr-2 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-blue-700 rounded-xl hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                       totoを編集
                     </button>
-                    <button onClick={() => handleDeleteTodo(todo)} className="w-50 px-10 py-4  mr-4 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-red-700 rounded-xl hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600">
+                    <button onClick={() => handleDeleteTodo(todo)} className="w-30 px-10 py-4  mr-2 text-base font-medium text-center text-white transition duration-500 ease-in-out transform bg-red-700 rounded-xl hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600">
                       todo削除！
                     </button>
                   </li>)}
